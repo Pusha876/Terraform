7c8631a2cf0b00a2f2547f3993b7660e882340dc
Jamie B. Pryce <djpusha876@gmail.com>
2025-08-12 16:37:36 -0400

    Day 2: Add initial Terraform configuration for Azure Storage Account

---
+++ Projects/day_3_Azure_Storage_Account/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 0.12"
+}
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "az400storageacct"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location # implicit dependency
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
